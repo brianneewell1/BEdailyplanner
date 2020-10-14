0d198544a6275644115aa17751d9c7f800f97cf4
Brianne <brianne.ewell1@gmail.com>
2020-10-13 21:06:31 -0600

    added current date and time at top

---
+++ scheduler.js
@@ -0,0 +1,26 @@
+
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+function generate_calendar() {
+    $("#nine").$
+
+    $("<div>").addClass()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
